24374b490c94dde9a23825ded863f5eee18cc484
AntonelloStefania <antonelloste94@gmail.com>
2023-06-25 19:37:48 +0200

    modifiche stile header

--- src/components/AppPokemonHeader.vue
+++ src/components/AppPokemonHeader.vue
@@ -26,15 +26,17 @@
 <template lang="">
 <div class="container">
     <div class="d-flex justify-content-between align-items-center">
-        <div>
-            <h1>Pokédex</h1>
-        </div>
-
-        <div class="d-flex">
-            <div class="water pkm-type"></div>
-            <div class="fire pkm-type"></div>
-            <div class="grass pkm-type"></div>
-            <div class="electric pkm-type"></div>
+        <div class="d-flex align-items-center">
+            <div>
+                <h1>Pokédex</h1>
+            </div>
+    
+            <div class="d-none d-lg-flex ms-3">
+                <div class="water pkm-type"><div class="internal-circle"></div></div>
+                <div class="fire pkm-type"><div class="internal-circle"></div></div>
+                <div class="grass pkm-type"><div class="internal-circle"></div></div>
+                <div class="electric pkm-type"><div class="internal-circle"></div></div>
+            </div>
         </div>
 
         <div>
@@ -57,12 +59,13 @@
 <style lang="scss" scoped>
 @use '../styles/partials/variables' as *;
 .pkm-type{
-    width: 70px;
-    height: 70px;
+    width: 50px;
+    height: 50px;
     border-radius: 5rem;
     border: 2px solid $pokemon_type_primary_color;
     margin: 1rem 0.5rem;
     cursor: pointer;
+    position: relative;
 
     &:hover{
         scale: 1.4;
@@ -71,22 +74,34 @@
 }
 .water{
     background: $pokemon_type_primary_color;
-    background: linear-gradient(123deg, $pokemon_type_primary_color 0%, rgba(0,9,129,1) 79%);
+    background: linear-gradient(0deg, $pokemon_type_primary_color 46%, rgba(0,0,0,1) 49%, rgba(0,0,0,1) 50%, rgb(20, 30, 164) 56%, rgba(38,50,236,1) 78%, rgba(97,105,241,1) 93%);
 }
 
 .grass{
     background: $pokemon_type_primary_color;
-    background: linear-gradient(123deg, $pokemon_type_primary_color 0%, rgba(0,120,30,1) 79%);
+    background: linear-gradient(0deg, $pokemon_type_primary_color 46%, rgba(0,0,0,1) 49%, rgba(0,0,0,1) 50%, rgb(6, 62, 12) 56%, rgb(22, 120, 9) 78%, rgb(15, 209, 99) 93%);
 }
 
 .electric{
     background: r$pokemon_type_primary_color;
-    background: linear-gradient(123deg, $pokemon_type_primary_color 0%, rgba(242,232,4,1) 79%); 
+    background: linear-gradient(0deg, $pokemon_type_primary_color 46%, rgba(0,0,0,1) 49%, rgba(0,0,0,1) 50%, rgb(149, 141, 3) 56%, rgb(210, 206, 14) 78%,rgb(251, 248, 75) 93%);
+
 }
 
 .fire{
     background: $pokemon_type_primary_color;
-    background: linear-gradient(123deg, $pokemon_type_primary_color 0%, rgba(255,19,19,1) 79%);
+    background: linear-gradient(0deg, $pokemon_type_primary_color 46%, rgba(0,0,0,1) 49%, rgba(0,0,0,1) 50%, rgb(125, 11, 3) 56%, rgb(183, 19, 8) 78%,rgb(251, 78, 66) 93%);
+}
+
+.internal-circle{
+    width: 15px;
+    height: 15px;
+    border-radius: 5rem;
+    background: radial-gradient(circle, rgba(255,249,249,1) 48%, rgba(0,0,0,1) 49%, rgba(57,56,56,1) 97%);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 }
 
 .select-bar{
